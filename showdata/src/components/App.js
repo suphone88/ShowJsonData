5747fa9eea2ba7b30b5ee2933ef3492fdcf91ee5
Dev Su Su <suphonemyint.la@gmail.com>
2022-11-08 09:51:10 +0630

    create api with db.jsonserver

---
+++ showdata/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ShowData from './ShowData';
+import '../style/App.css';
+
+const App = () => {
+    return(
+        <div className="ui container"> <ShowData /> </div>
+    );
+};
+
+export default App;
